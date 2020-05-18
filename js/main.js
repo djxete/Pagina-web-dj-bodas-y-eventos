d14a1f442b2d17ddfa00444e72df1fcf4aff82c1
Ignacio Herraez <ignacio_hg_87@hotmail.com>
2020-05-18 17:52:30 +0200

    he hecho la seccion de dj xete debajo de testimoniales y la seccion de preguntas frecuentes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 // BARRA DE NAVEGACIÓN Y MENU DESPLEGABLE //
 
 $(document).ready(function () {
@@ -47,48 +46,129 @@ function normalidad(e) {
 };
 
 
-
 $(document).ready(function () {
 
-    $("#menuNav").hide();
+    $(".menuDesplegable").hide();
 
-    document.getElementById("barraHeader").addEventListener("click", abrir, false);
-    document.getElementById("barraHeader").addEventListener("mouseover", brillo, false);
-    document.getElementById("barraHeader").addEventListener("mouseout", normal, false);
+    $("#barraHeader").click(function () {
+
+        $(".menuDesplegable").slideToggle(1000);
+        $(this).css("color", "#fe4918");
+
+    });
+
+    $("#barraHeader").mouseover(function(){
+
+        $(this).css("color", "#fe4918");
+    })
+
+    $("#barraHeader").mouseout(function(){
+
+        $(this).css("color", "#ffffff");
+    })
+
 
 
 });
 
-function abrir() {
-
-   
-
-    $("#menuNav").toggle(2000);
-
-};
 
 
 
-function brillo(e) {
+// $(document).ready(function () {
 
-    if (e.target == barraHeader) {
+//     $("#menuNav").hide();
 
-        $("#barraHeader").addClass("destacar");
-        var borderFondo = $("#barraHeader").css("border-bottom", "2px solid #f83c08");
-    }
+//     document.getElementById("barraHeader").addEventListener("click", abrir, false);
+//     document.getElementById("barraHeader").addEventListener("mouseover", brillo, false);
+//     document.getElementById("barraHeader").addEventListener("mouseout", normal, false);
 
 
-};
+// });
 
-function normal(e) {
-
-    if (e.target === barraHeader) {
-
-        $("#barraHeader").removeClass("destacar");
-        $("#barraHeader").css("border-bottom", "none");
-
-    }
-};
+// function abrir() {
 
 
 
+//     $("#menuNav").toggle(2000);
+
+// };
+
+
+
+// function brillo(e) {
+
+//     if (e.target == barraHeader) {
+
+//         $("#barraHeader").addClass("destacar");
+//         var borderFondo = $("#barraHeader").css("border-bottom", "2px solid #f83c08");
+//     }
+
+
+// };
+
+// function normal(e) {
+
+//     if (e.target === barraHeader) {
+
+//         $("#barraHeader").removeClass("destacar");
+//         $("#barraHeader").css("border-bottom", "none");
+
+//     }
+// };
+
+
+
+// PREGUNTAS FRECUENTES //
+
+
+$(document).ready(function () {
+
+    $(".pf_preguntas #respuesta").hide();
+
+    $(".pf_suma").click(function () {
+
+        $(this).next("#respuesta").slideToggle(1000);
+        $(this).toggleClass("pf_resta");
+
+    })
+
+
+
+
+});
+
+
+
+// $(document).ready(function () {
+//     $(".pf_preguntas #respuesta").hide();
+//     $(".pf_suma").click(function () {
+//         $(this).next("#respuesta").slideToggle(1000); //apuntamos al siguiente elemento del this
+//         $(this).toggleClass("pf_resta"); //cambiamos la clase para cambiar la imagen 
+
+//     });
+// });
+
+
+// $(document).ready(function(){
+
+//     $(".pf_preguntas #respuesta").hide();
+
+//     let icono=true;
+
+//     $(".pf_suma").click(function(){
+
+//         $(this).next("#respuesta").fadeToggle(500);
+
+//         if(icono){
+
+//             $(this).addClass("pf_resta");
+//             icono=false;
+//         } else{
+
+//             $(this).removeClass("pf_resta");
+//             icono=true;
+//         }
+
+//     });
+
+// });
