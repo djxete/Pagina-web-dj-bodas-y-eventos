a60e0ffe2cbc74e792d5a0b12e5557fd88edf8e0
Ignacio Herraez <ignacio_hg_87@hotmail.com>
2020-05-19 19:41:18 +0200

    -cambie todo el header y lo puse con flex box dividido en tres elelementos

--- js/main.js
+++ js/main.js
@@ -1,49 +1,51 @@
+
+
 // BARRA DE NAVEGACIÓN Y MENU DESPLEGABLE //
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    var enlaceRedesSociales = document.querySelectorAll("a,i");
+//     var enlaceRedesSociales = document.querySelectorAll("a,i");
 
-    for (var i = 0; i < enlaceRedesSociales.length; i++) {
+//     for (var i = 0; i < enlaceRedesSociales.length; i++) {
 
-        enlaceRedesSociales[i].addEventListener("mouseover", iluminar, false);
+//         enlaceRedesSociales[i].addEventListener("mouseover", iluminar, false);
 
-        enlaceRedesSociales[i].addEventListener("mouseout", normalidad, false);
-    };
+//         enlaceRedesSociales[i].addEventListener("mouseout", normalidad, false);
+//     };
 
-});
+// });
 
-function iluminar(e) {
+// function iluminar(e) {
 
-    if (e.target == facebook) {
+//     if (e.target == facebook) {
 
-        $("#facebook").addClass("destacar");
-        var borderFondo = $("#facebook").css("border-bottom");
-        $("#facebook").css("border-bottom", "3px solid red");
+//         $("#facebook").addClass("destacar");
+//         var borderFondo = $("#facebook").css("border-bottom");
+//         $("#facebook").css("border-bottom", "3px solid red");
 
 
 
-    } else if (e.target === instagram) {
+//     } else if (e.target === instagram) {
 
-        $("#instagram").addClass("destacar");
-        var borderFondo = $("#instagram").css("border");
-        $("#instagram").css("border-bottom", "3px solid red");
+//         $("#instagram").addClass("destacar");
+//         var borderFondo = $("#instagram").css("border");
+//         $("#instagram").css("border-bottom", "3px solid red");
 
-    }
-};
+//     }
+// };
 
-function normalidad(e) {
+// function normalidad(e) {
 
-    if (e.target === facebook) {
+//     if (e.target === facebook) {
 
-        $("#facebook").removeClass("destacar");
-        $("#facebook").css("border-bottom", "none");
+//         $("#facebook").removeClass("destacar");
+//         $("#facebook").css("border-bottom", "none");
 
-    } else if (e.target == instagram) {
-        $("#instagram").removeClass("destacar");
-        $("#instagram").css("border-bottom", "none");
-    };
-};
+//     } else if (e.target == instagram) {
+//         $("#instagram").removeClass("destacar");
+//         $("#instagram").css("border-bottom", "none");
+//     };
+// };
 
 
 $(document).ready(function () {
