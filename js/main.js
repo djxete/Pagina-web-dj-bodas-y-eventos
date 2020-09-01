7ce74424d6dccd2e9b40538bcf1dbd3dd7b7f19c
Ignacio Herraez <ignacio_hg_87@hotmail.com>
2020-09-01 23:25:53 +0200

    hemos hecho mas validaciones del formulario

--- js/main.js
+++ js/main.js
@@ -57,7 +57,7 @@ $(document).ready(function () {
 
     })
 
- 
+
 
 
     //     2º cada imagen cuando nos situamos encima de cada una, se tiene que agrandar la imagen,cambiar el color y quitar la pestaña de abajo
@@ -68,26 +68,31 @@ $(document).ready(function () {
 
     let cuadradoImagenes = document.querySelectorAll(".eventoEnlace");
 
-    for(let i = 0; i < imgs.length ; i++){
+    for (let i = 0; i < imgs.length; i++) {
 
         imgs[i].addEventListener("mouseover", agrandar);
         imgs[i].addEventListener("mouseout", normalidad);
 
-        function agrandar(){
+        function agrandar() {
             imgs[i].style.transform = "scale(1.4)";
             imgs[i].style.filter = "none";
             cuadradoImagenes[i].style.bottom = "-100px";
         }
-        function normalidad(){
+
+        function normalidad() {
             imgs[i].style.transform = "scale(1)";
-            imgs[i].style.filter ="grayscale(1)";
+            imgs[i].style.filter = "grayscale(1)";
             cuadradoImagenes[i].style.bottom = "0px";
         }
 
 
     }
 
-   
+
+
+
+
+
 
 
 
@@ -100,107 +105,172 @@ $(document).ready(function () {
     let telefonoForm = document.getElementById("telefono__value");
     let textareaForm = document.getElementById("textarea__value");
     let dateForm = document.getElementById("dia__value");
+    let dateForm2 = document.getElementById("dia__value2");
     let dateFormContact = document.getElementById("dia__value__contacto");
     let hourFormContact = document.getElementById("hora__value__contacto")
     let isCheckedTelefono = document.querySelectorAll(".input_form-radio")[0].checked;
     let isCheckedEmail = document.querySelectorAll(".input_form-radio")[1].checked;
-    
-    
+
+
+
 
     //funciones
 
-    formulario.addEventListener("submit", function(){
+    formulario.addEventListener("submit", function () {
 
-    //campo texto
-    
-    if(nameForm.value == 0 || nameForm.value == null || /^\s+$/.test(nameForm.value) || nameForm.value.length > 50 || !isNaN(nameForm.value) ) {
+        //campo texto
 
-        nameForm.classList.add("inputIncorrecto");
-        document.querySelector(".name__p").style.display = "block";
+        if (nameForm.value == 0 || nameForm.value == null || /^\s+$/.test(nameForm.value) || nameForm.value.length > 50 || !isNaN(nameForm.value)) {
 
-    } else{
-        nameForm.classList.remove("inputIncorrecto");
-        document.querySelector(".name__p").style.display = "none";
-    }
-   
-    //campo email
+            nameForm.classList.add("inputIncorrecto");
+            document.querySelector(".name__p").style.display = "block";
 
-    if(emailForm.value == 0 || emailForm.value == null || /^\s+$/.test(emailForm.value) || emailForm.value.length > 50 || !/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/ ) {
-        emailForm.classList.add("inputIncorrecto");
-        document.querySelector(".email__p").style.display = "block";
-    } else{
-        emailForm.classList.remove("inputIncorrecto");
-        document.querySelector(".email__p").style.display = "none";
-    }
-   
-    //campo telefono
+        } else {
+            nameForm.classList.remove("inputIncorrecto");
+            document.querySelector(".name__p").style.display = "none";
+        }
 
-    if(telefonoForm.value == 0 || telefonoForm.value == null || /^\s+$/.test(telefonoForm.value) || telefonoForm.value.length > 20 || isNaN(telefonoForm.value) ) {
+        //campo email
 
-        telefonoForm.classList.add("inputIncorrecto");
-        document.querySelector(".telefono__p").style.display = "block";
-    } else{
-        telefonoForm.classList.remove("inputIncorrecto");
-        document.querySelector(".telefono__p").style.display = "none";
-    }
-   
-    //campo textarea
+        if (emailForm.value == 0 || emailForm.value == null || /^\s+$/.test(emailForm.value) || emailForm.value.length > 50 || !/^([\da-z_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/) {
+            emailForm.classList.add("inputIncorrecto");
+            document.querySelector(".email__p").style.display = "block";
+        } else {
+            emailForm.classList.remove("inputIncorrecto");
+            document.querySelector(".email__p").style.display = "none";
+        }
 
-    if(textareaForm.value.length > 200 ){
+        //campo telefono
 
-        textareaForm.classList.add("inputIncorrecto");
-        document.querySelector(".textarea__p").style.display = "block";
+        if (telefonoForm.value == 0 || telefonoForm.value == null || /^\s+$/.test(telefonoForm.value) || telefonoForm.value.length > 20 || isNaN(telefonoForm.value)) {
 
-    } else{
-        textareaForm.classList.remove("inputIncorrecto");
-        document.querySelector(".textarea__p").style.display = "none";
-    }
+            telefonoForm.classList.add("inputIncorrecto");
+            document.querySelector(".telefono__p").style.display = "block";
+        } else {
+            telefonoForm.classList.remove("inputIncorrecto");
+            document.querySelector(".telefono__p").style.display = "none";
+        }
 
-    //campo fecha
+        //campo textarea
 
-    let nuevaFecha= new Date(); //fecha de hoy
-    
-    let actualDate= new Date(dateForm.value); //fecha que introduce el cliente
-    
-   
-    nuevaFecha.setHours(0,0,0,0);
-    actualDate.setHours(0,0,0,0);
+        if (textareaForm.value.length > 200) {
 
-    if((nuevaFecha < actualDate) || (dateForm.value == 0)) {
-        document.querySelector(".date__p").style.display = "none";
-        dateForm.classList.remove("inputIncorrecto");
+            textareaForm.classList.add("inputIncorrecto");
+            document.querySelector(".textarea__p").style.display = "block";
 
-    }  else{
-        
-        dateForm.classList.add("inputIncorrecto");
-        
-    }
+        } else {
+            textareaForm.classList.remove("inputIncorrecto");
+            document.querySelector(".textarea__p").style.display = "none";
+        }
 
-    //checkbox como desea ser contactado
+        //campo fecha
 
-    if((!document.querySelectorAll(".input_form-radio")[0].checked) && (!document.querySelectorAll(".input_form-radio")[1].checked)){
+        let nuevaFecha = new Date(); //fecha de hoy
 
-    
-    document.querySelector(".radio__block").style.display = "block";
-        
-    } else if((document.querySelectorAll(".input_form-radio")[0].checked) && (document.querySelectorAll(".input_form-radio")[1].checked)){
-
-      
-        document.querySelector(".radio__block").style.display = "block";
-        
-
-    } else{
-    
-    document.querySelector(".radio__block").style.display = "none";
-    }
-    
-    
-})
-   
-   
-
-    
+        let actualDate = new Date(dateForm.value); //fecha que introduce el cliente
 
 
-    
+        nuevaFecha.setHours(0, 0, 0, 0);
+        actualDate.setHours(0, 0, 0, 0);
+
+        if ((nuevaFecha < actualDate) || (dateForm.value == 0)) {
+            document.querySelector(".date__p").style.display = "none";
+            dateForm.classList.remove("inputIncorrecto");
+
+        } else {
+
+            dateForm.classList.add("inputIncorrecto");
+            document.querySelector(".date__p").style.display = "block";
+
+        }
+
+        //checkbox como desea ser contactado y si elegimos teléfono hay que elegir una fecha y hora
+
+        if ((!document.querySelectorAll(".input_form-radio")[0].checked) && (!document.querySelectorAll(".input_form-radio")[1].checked)) {
+
+            document.querySelector(".radio__block").style.display = "block";
+
+
+        } else if ((document.querySelectorAll(".input_form-radio")[0].checked) && (document.querySelectorAll(".input_form-radio")[1].checked)) {
+
+            document.querySelector(".radio__block").style.display = "block";
+
+
+        } else if ((document.querySelectorAll(".input_form-radio")[1].checked)) {
+            document.querySelector(".radio__block").style.display = "none";
+
+        } else if ((document.querySelectorAll(".input_form-radio")[0].checked)) {
+            // si hemos seleccionado solo la opción de teléfono
+            
+            document.querySelector(".radio__block").style.display = "none";
+
+
+            let nuevaFecha2 = new Date(); //fecha de hoy
+
+            let actualDate2 = new Date(dateForm2.value); //fecha que introduce el cliente si el teléfono fue elegido
+
+            nuevaFecha2.setHours(0, 0, 0, 0);
+            actualDate2.setHours(0, 0, 0, 0);
+
+            if ((dateForm2.value == 0) || (nuevaFecha2 < actualDate2)) {
+                
+                document.querySelectorAll(".date__p")[1].style.display = "block";
+            } 
+            else{
+                document.querySelectorAll(".date__p")[1].style.display = "none";
+            }
+            // else if(hourFormContact.value == 0){
+                
+            //     document.querySelectorAll(".date__p")[2].style.display = "block";
+
+            // }
+            
+            // else {
+
+            //     if ((nuevaFecha2 < actualDate2)) {
+            //         document.querySelectorAll(".date__p")[1].style.display = "none";
+            //         dateForm2.classList.remove("inputIncorrecto");
+
+            //     } else {
+
+            //         dateForm2.classList.add("inputIncorrecto");
+            //         document.querySelectorAll(".date__p")[1].style.display = "block";
+
+            //     }
+
+            // }
+
+
+
+        }
+
+
+        //si eligió teléfono, eliga fecha y hora
+
+
+        /*if (document.querySelectorAll(".input_form-radio")[0].checked) {
+
+            
+
+
+        }*/
+
+
+
+
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
 });
